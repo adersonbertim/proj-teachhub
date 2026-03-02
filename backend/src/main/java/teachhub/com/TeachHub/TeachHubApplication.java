da6d653d9520f23205212d2b5d067356ac8024cc
Aderson Bertim <adersonbertim@gmail.com>
2026-03-02 01:57:59 -0300

    add: .env file and security to api's

--- backend/src/main/java/teachhub/com/TeachHub/TeachHubApplication.java
+++ backend/src/main/java/teachhub/com/TeachHub/TeachHubApplication.java
@@ -1,5 +1,6 @@
 package teachhub.com.TeachHub;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TeachHubApplication {
 
 	public static void main(String[] args) {
+		// 1. Carrega as variáveis do arquivo .env
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./") // Procura na raiz do projeto
+				.ignoreIfMissing()
+				.load();
+
+		// 2. Injeta cada variável do .env no sistema para o Spring encontrar
+		dotenv.entries().forEach(entry -> {
+			System.setProperty(entry.getKey(), entry.getValue());
+		});
+
+		// 3. Agora sim, inicia o Spring
 		SpringApplication.run(TeachHubApplication.class, args);
 	}
-
 }
